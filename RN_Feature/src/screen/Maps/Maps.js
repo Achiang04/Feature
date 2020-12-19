daf1c5291b5acc2540f6d324ae3b446531460dce
achiang <jecky_hindrawan123@yahoo.com>
2020-12-19 21:09:05 +0700

    nothing shown

--- RN_Feature/src/screen/Maps/Maps.js
+++ RN_Feature/src/screen/Maps/Maps.js
@@ -1,10 +1,32 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 export default function Maps() {
   return (
     <View>
       <Text>Maps Screen</Text>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}></MapView>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
